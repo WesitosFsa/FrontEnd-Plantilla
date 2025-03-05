4bb78615fe3f954d80773e4cc69bd2a3530b2934
WesitosFsa <mateo.garzon@epn.edu.ec>
2025-03-05 10:42:08 -0500

    limpiar capacidad

--- src/components/RegistrarModulo1.jsx
+++ src/components/RegistrarModulo1.jsx
@@ -12,7 +12,7 @@ const RegistrarModulo1 = () => {
   const [cedula, setcedula] = useState('');
   const [nombre, setNombre] = useState('');
   const [ubicacion, setUbicacion] = useState('');
-  const [capacidad, setCapacidad] = useState();
+  const [capacidad, setCapacidad] = useState('');
   const [descripcion, setDescripcion] = useState('');
 
   const [mostrarModal, setMostrarModal] = useState(false); // Estado para controlar el modal
